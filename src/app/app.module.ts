c360f63b3a51f529cbebfd2674ce8e3ced2f882b
QuantumlyTangled <ethoarx@gmail.com>
2020-08-20 09:05:18 +0200

    refactor: file locations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AnimeCardComponent } from './anime-card/anime-card.component';
+import { AnimeCardComponent } from './info/card/anime-card/anime-card.component';
 
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
